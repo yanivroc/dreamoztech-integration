8220e7d4f8ec065861cba4789aa8c852c6cc65ff
yanivroc <rocketvinay@live.com>
2025-07-08 18:00:49 +1000

    cookies update

---
+++ OvenBites/Api/MenuItemModel.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace OvenBites.Api
+{
+    public class MenuItemModel
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("http")]
+        public string Http { get; set; }
+    }
+}
